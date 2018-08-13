f00a7be48814c9671f8dfa1ef78cabf513b46582
brence <31331626+brence@users.noreply.github.com>
2018-08-12 20:16:08 -0500

    Optimized
    Optimized.

--- Percolation.java
+++ Percolation.java
@@ -1,15 +1,15 @@
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-    private WeightedQuickUnionUF grid;
-    private int gridsize;
-    private int top;
-    private int bottom;
+    private final WeightedQuickUnionUF grid;
+    private final int gridsize;
+    private final int top;
+    private final int bottom;
     private boolean[][] opened;
+    private int gnOpenSites;
     
     public Percolation(int n) {
+        if (n <= 0) throw new IllegalArgumentException("Grid size needs to be bigger than 0!");
         gridsize = n;
         
         grid = new WeightedQuickUnionUF(gridsize * gridsize + 2);
@@ -17,68 +17,75 @@ public class Percolation {
         bottom = gridsize * gridsize + 1;
         
         // top connects with the first row, bottom connects with the last row
-        for(int i=1; i<=gridsize; i++) {
+        for (int i = 1; i <= gridsize; i++) {
             grid.union(top, i);
             grid.union(bottom, gridsize * gridsize + 1 - i);
         }
         
         // Initially every site is blocked
         opened = new boolean[gridsize+1][gridsize+1];
-        for(int i=1; i<=gridsize; i++) {
-            for(int j=1; j<=gridsize; j++) {
+        for (int i = 1; i <= gridsize; i++) {
+            for (int j = 1; j <= gridsize; j++) {
                 opened[i][j] = false;
             }
         }
+        gnOpenSites = 0;
     }
     
     public void open(int row, int col) {
-        opened[row][col] = true;
-        // up
-        if(row>1 && isOpen(row-1, col)) {
-            grid.union(getIndex(row, col), getIndex(row-1, col));
-        }
-        // bottom
-        if(row<gridsize && isOpen(row+1, col)) {
-            grid.union(getIndex(row, col), getIndex(row+1, col));
-        }
-        // left
-        if(col>1 && isOpen(row, col-1)) {
-            grid.union(getIndex(row, col), getIndex(row, col-1));
-        }
-        // right
-        if(col<gridsize && isOpen(row, col+1)) {
-            grid.union(getIndex(row, col), getIndex(row, col+1));
+        if (row < 1 || row > gridsize) throw new IllegalArgumentException("Row must be between 1 and grid size!");
+        if (col < 1 || col > gridsize) throw new IllegalArgumentException("Column must be between 1 and grid size!");
+        if (!opened[row][col]) {
+            opened[row][col] = true;
+            gnOpenSites++;
+            // up
+            if (row > 1 && isOpen(row - 1, col)) {
+                grid.union(getIndex(row, col), getIndex(row - 1, col));
+            }
+            // bottom
+            if (row < gridsize && isOpen(row + 1, col)) {
+                grid.union(getIndex(row, col), getIndex(row + 1, col));
+            }
+            // left
+            if (col > 1 && isOpen(row, col - 1)) {
+                grid.union(getIndex(row, col), getIndex(row, col - 1));
+            }
+            // right
+            if (col < gridsize && isOpen(row, col + 1)) {
+                grid.union(getIndex(row, col), getIndex(row, col + 1));
+            }
         }
     }
     
     public boolean isOpen(int row, int col) {
+        if (row < 1 || row > gridsize) throw new IllegalArgumentException("Row must be between 1 and grid size!");
+        if (col < 1 || col > gridsize) throw new IllegalArgumentException("Column must be between 1 and grid size!");
         return opened[row][col];
     }
         
     public boolean isFull(int row, int col) {
+        if (row < 1 || row > gridsize) throw new IllegalArgumentException("Row must be between 1 and grid size!");
+        if (col < 1 || col > gridsize) throw new IllegalArgumentException("Column must be between 1 and grid size!");
+        if (!isOpen(row, col)) return false;
         return grid.connected(top, getIndex(row, col));
     }
     
     public int numberOfOpenSites() {
-        int nOpenSites = 0;
-        
-        for(int i=1; i<=gridsize; i++) {
-            for(int j=1; j<=gridsize; j++) {
-                if(opened[i][j]) nOpenSites++;
-            }
-        }
-        
-        return nOpenSites;
+        return gnOpenSites;
     }
     
     public boolean percolates() {
+        if (numberOfOpenSites() == 0) return false;
         return grid.connected(top, bottom);
     }
     
     private int getIndex(int row, int col) {
+        if (row < 1 || row > gridsize) throw new IllegalArgumentException("Row must be between 1 and grid size!");
+        if (col < 1 || col > gridsize) throw new IllegalArgumentException("Column must be between 1 and grid size!");
         return (row - 1) * gridsize + col;
     }
     
     public static void main(String[] args) {
+        // Leave it to add future test clients
     }
 }
--- PercolationStats.java
+++ PercolationStats.java
@@ -1,58 +1,70 @@
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
-import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class PercolationStats {
-    private double[] gdThresholds; 
-    private int gnTrialCount;
+    private static final double CONFIDENCE_95 = 1.96;
+    private final double[] gdThresholds; 
+    private final int gnTrialCount;
+    private double gLastMean = 0;
+    private double gLastStddev = 0;
     
     public PercolationStats(int nSize, int nTrialCount) {
-        if(nSize<=0) throw new IllegalArgumentException("Grid size needs to be bigger than 0!");
-        if(nTrialCount<=0) throw new IllegalArgumentException("Trial times needs to be bigger than 0!");
+        if (nSize <= 0) throw new IllegalArgumentException("Grid size needs to be bigger than 0!");
+        if (nTrialCount <= 0) throw new IllegalArgumentException("Trial times needs to be bigger than 0!");
         
-        private Percolation oPercalation;
+        Percolation oPercalation;
+        gnTrialCount = nTrialCount;
+        gdThresholds = new double[gnTrialCount];
         
-        for(int i=0; i<nTrialCount; i++) {
+        for (int i = 0; i < nTrialCount; i++) {
+            // System.out.println("Trial time: " + (i+1));
             oPercalation = new Percolation(nSize);
             int nOpenSite = 0;
-            while(!oPercalation.percolates()) {
+            while (!oPercalation.percolates()) {
+                // System.out.println("  Open Sites: " + nOpenSite);
                 int nRow = StdRandom.uniform(1, nSize + 1);
                 int nCol = StdRandom.uniform(1, nSize + 1);
-                if(!oPercalation.isOpen(nRow, nCol)) {
+                if (!oPercalation.isOpen(nRow, nCol)) {
                     oPercalation.open(nRow, nCol);
                     nOpenSite++;
                 }
-            }
-            double dThreshold = (double) nOpenSite / (nSize * nSize);
-            gdgThresholds[i] = dThreshold;
+            }            
+            double dThreshold = ((double) nOpenSite) / (nSize * nSize);
+            // System.out.println("Immediate Threshold: " + dThreshold);
+            gdThresholds[i] = dThreshold;
         }
+        // for(int i=0; i<nTrialCount; i++) {
+        //    System.out.println("Threshold: " + gdThresholds[i]);
+        // }
     }
     
     public double mean() {
-        return StdStats.mean(gdThresholds);
+        gLastMean = StdStats.mean(gdThresholds);
+        return gLastMean;
     }
     
     public double stddev() {
-        return StdStats.stddev(gdThresholds);
+        gLastStddev = StdStats.stddev(gdThresholds);
+        return gLastStddev;
     }
     
     public double confidenceLo() {
-        return mean() - ((1.96 * stddev()) / Math.sqrt(gnTrialCount));
+        return gLastMean - ((CONFIDENCE_95 * gLastStddev) / Math.sqrt(gnTrialCount));
     }
     
     public double confidenceHi() {
-        return mean() + ((1.96 * stddev()) / Math.sqrt(gnTrialCount));
+        return gLastMean + ((CONFIDENCE_95 * gLastStddev) / Math.sqrt(gnTrialCount));
     }
     
     
     public static void main(String[] args) {
         int nSize = Integer.parseInt(args[0]);
-        gnTrialCount = Integer.parseInt(args[1]);
-        PercolationStats oPercolationStats = new PercolationStats(nSize, gnTrialCount);
+        int nTrialCount = Integer.parseInt(args[1]);
+        PercolationStats oPercolationStats = new PercolationStats(nSize, nTrialCount);
         
-        System.out.println("mean                    = " + mean());
-        System.out.println("stddev                  = " + stddev());
-        System.out.println("95% confidence interval = [" + confidenceLo() + ", " + confidenceHi() + "]");
+        System.out.println("mean                    = " + oPercolationStats.mean());
+        System.out.println("stddev                  = " + oPercolationStats.stddev());
+        System.out.println("95% confidence interval = [" + oPercolationStats.confidenceLo() + ", " + oPercolationStats.confidenceHi() + "]");
     }
 }
         
